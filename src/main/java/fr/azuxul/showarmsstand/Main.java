4bb73fd020e48434c9b73d87f3f4182284788cc1
azuxul <contact@azuxul.fr>
2016-01-23 18:49:03 +0100

    Update to 1.2 for Minecraft 1.8.9

---
+++ src/main/java/fr/azuxul/showarmsstand/Main.java
@@ -0,0 +1,40 @@
+package fr.azuxul.showarmsstand;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+@Mod(modid = Main.MODID, version = Main.VERSION, name = Main.NAME)
+
+public class Main {
+
+    public static final String MODID = "showarmsstand";
+    public static final String NAME = "ShowArmsStand";
+    public static final String VERSION = "1.1";
+    public static final Item itemShowingArmsArmorStand = new ShowingArmsArmorStand();
+    public static final Item itemSmallShowingArmsArmorStand = new SmallShowingArmsArmorStand();
+
+    @EventHandler
+    public static void init(FMLInitializationEvent event) {
+
+        GameRegistry.registerItem(itemShowingArmsArmorStand, "showingarmsarmorstand");
+        GameRegistry.registerItem(itemSmallShowingArmsArmorStand, "smallshowingarmsarmorstand");
+
+        GameRegistry.addShapedRecipe(new ItemStack(itemShowingArmsArmorStand), "BAB", 'A', Items.armor_stand, 'B', Items.stick);
+        GameRegistry.addShapedRecipe(new ItemStack(itemSmallShowingArmsArmorStand), "A", 'A', Items.armor_stand);
+        GameRegistry.addShapedRecipe(new ItemStack(Items.armor_stand), "A", 'A', itemShowingArmsArmorStand);
+        GameRegistry.addShapedRecipe(new ItemStack(Items.armor_stand, 0, 2), "A", 'A', itemSmallShowingArmsArmorStand);
+
+        if (event.getSide().isClient()) {
+
+            Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(itemSmallShowingArmsArmorStand, 0, new ModelResourceLocation(Main.MODID + ":smallshowingarmsarmorstand", "inventory"));
+            Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(itemShowingArmsArmorStand, 0, new ModelResourceLocation(Main.MODID + ":showingarmsarmorstand", "inventory"));
+        }
+    }
+}
---
+++ src/main/java/fr/azuxul/showarmsstand/Utils.java
@@ -0,0 +1,32 @@
+package fr.azuxul.showarmsstand;
+
+import net.minecraft.entity.item.EntityArmorStand;
+import net.minecraft.util.Rotations;
+
+import java.util.Random;
+
+/**
+ * Utils
+ *
+ * @author Azuxul
+ * @version 1.0
+ */
+public class Utils {
+
+    private Utils() {
+
+    }
+
+    public static void applyRandomRotations(EntityArmorStand armorStand, Random rand) {
+
+        Rotations rotations = armorStand.getHeadRotation();
+        float f = rand.nextFloat() * 5.0F;
+        float f1 = rand.nextFloat() * 20.0F - 10.0F;
+        Rotations rotations1 = new Rotations(rotations.getX() + f, rotations.getY() + f1, rotations.getZ());
+        armorStand.setHeadRotation(rotations1);
+        rotations = armorStand.getBodyRotation();
+        f = rand.nextFloat() * 10.0F - 5.0F;
+        rotations1 = new Rotations(rotations.getX(), rotations.getY() + f, rotations.getZ());
+        armorStand.setBodyRotation(rotations1);
+    }
+}
