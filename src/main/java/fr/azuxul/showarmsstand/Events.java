a0d155653d513a4e8aac1fa0b75425f9e7e6c431
azuxul <contact@azuxul.fr>
2017-04-05 22:20:16 +0200

    Now offhand items can be removed if mainhand is empty. Disabled slots disable the wand.

--- src/main/java/fr/azuxul/showarmsstand/Events.java
+++ src/main/java/fr/azuxul/showarmsstand/Events.java
@@ -22,13 +22,7 @@ public class Events {
 
         ItemStack item = ((EntityPlayer) event.getEntity()).getHeldItemMainhand();
 
-        if (!(event.getTarget() instanceof EntityArmorStand) || item == null || !ShowArmsStand.itemSwitchHandWand.equals(item.getItem())) {
-            return;
-        }
-
-        event.setCanceled(true);
-
-        if (event.getHand() != EnumHand.MAIN_HAND) {
+        if (!(event.getTarget() instanceof EntityArmorStand)) {
             return;
         }
 
@@ -38,17 +32,52 @@ public class Events {
 
         entityArmorStand.writeEntityToNBT(nbtTagCompound);
 
+        if (item == null) {
 
-        if (nbtTagCompound.getBoolean("ShowArms")) {
+            if (entityArmorStand.getHeldItemMainhand() == null && entityArmorStand.getHeldItemOffhand() != null && !hasDisabledSlots(nbtTagCompound, EntityEquipmentSlot.MAINHAND, true, true, false)) {
 
-            ItemStack hand1 = entityArmorStand.getItemStackFromSlot(EntityEquipmentSlot.MAINHAND);
-            ItemStack hand2 = entityArmorStand.getItemStackFromSlot(EntityEquipmentSlot.OFFHAND);
+                ((EntityPlayer) event.getEntity()).setHeldItem(event.getHand(), entityArmorStand.getHeldItemOffhand());
+                entityArmorStand.setHeldItem(EnumHand.OFF_HAND, null);
+            }
 
-            entityArmorStand.setItemStackToSlot(EntityEquipmentSlot.MAINHAND, hand2);
-            entityArmorStand.setItemStackToSlot(EntityEquipmentSlot.OFFHAND, hand1);
 
+        } else if (ShowArmsStand.itemSwitchHandWand.equals(item.getItem()) && !hasDisabledSlots(nbtTagCompound, EntityEquipmentSlot.MAINHAND, true, true, true) && !hasDisabledSlots(nbtTagCompound, EntityEquipmentSlot.OFFHAND, true, true, true)) {
+
+            event.setCanceled(true);
+
+            if (event.getHand() != EnumHand.MAIN_HAND) {
+                return;
+            }
+
+            if (nbtTagCompound.getBoolean("ShowArms")) {
+
+                ItemStack hand1 = entityArmorStand.getItemStackFromSlot(EntityEquipmentSlot.MAINHAND);
+                ItemStack hand2 = entityArmorStand.getItemStackFromSlot(EntityEquipmentSlot.OFFHAND);
+
+                entityArmorStand.setItemStackToSlot(EntityEquipmentSlot.MAINHAND, hand2);
+                entityArmorStand.setItemStackToSlot(EntityEquipmentSlot.OFFHAND, hand1);
+
+            }
         }
 
+    }
+
+    private boolean hasDisabledSlots(NBTTagCompound nbtTagCompound, EntityEquipmentSlot slot, boolean remove, boolean replace, boolean place) {
+
+        if (nbtTagCompound.hasKey("DisabledSlots")) {
+            int disabledSlots = nbtTagCompound.getInteger("DisabledSlots");
+
+            if ((disabledSlots & 1 << slot.getIndex()) != 0 && remove)
+                return true;
+
+            if ((disabledSlots & 1 << slot.getIndex() + 8) != 0 && replace)
+                return true;
+
+            if ((disabledSlots & 1 << slot.getIndex() + 16) != 0 && place)
+                return true;
+        }
+
+        return false;
     }
 
 }
